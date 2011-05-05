9df42678153fad9d5adbfb40748a42dfdadc3cef
stanley lieber <stanley.lieber@gmail.com>
2011-05-05 13:35:17 +0000

    /usr/glenda/bin/rc/riostart: window -scroll

---
+++ sys/src/9/boot/boot.h
@@ -0,0 +1,18 @@
+enum {
+	Debug = 0,
+};
+
+extern char*	bootdisk;
+extern char*	rootdir;
+extern int		(*cfs)(int);
+extern int		cpuflag;
+extern char		cputype[];
+extern int		fflag;
+extern int		kflag;
+
+extern void fatal(char*);
+extern int	readfile(char*, char*, int);
+extern void run(char*, ...);
+extern void	setenv(char*, char*, int);
+extern int	writefile(char*, char*, int);
+extern void	boot(int, char **);
