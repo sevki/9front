0685dc291ba35502f8fa2e64576731b0c831ae52
stanley lieber <stanley.lieber@gmail.com>
2012-03-16 03:24:14 -0500

    usps: fix broken api call, filter out html junk

---
+++ sys/src/cmd/paint.c
@@ -0,0 +1,150 @@
+#include <u.h>
+#include <libc.h>
+#include <draw.h>
+#include <event.h>
+
+#define NCOLORS 6
+
+Image	*colors[NCOLORS];
+
+void
+eresized(int)
+{
+	if(getwindow(display, Refnone) < 0)
+		sysfatal("resize failed");
+}
+
+int
+loadimg(char *name)
+{
+	Image *b;
+	int fd;
+
+	if((fd = open(name, OREAD)) < 0)
+		return -1;
+	if((b = readimage(display, fd, 0)) == nil){
+		close(fd);
+		return -1;
+	}
+	draw(screen, screen->r, b, 0, b->r.min);
+	flushimage(display, 1);
+	freeimage(b);
+	close(fd);
+	return 0;
+}
+
+int
+saveimg(char *name)
+{
+	int fd;
+
+	if((fd = create(name, OWRITE|OTRUNC, 0666)) < 0)
+		return -1;
+	writeimage(fd, screen, 0);
+	close(fd);
+	return 0;
+}
+
+void
+main(int argc, char *argv[])
+{
+	Event e;
+	Point last;
+	int b = 1;
+	int c = 0;
+	int cn, f;
+	int haslast = 0;
+	char brush[128];
+	char color[NCOLORS];
+	char file[128];
+	char fill[NCOLORS];
+	
+	if(initdraw(0, 0, "paint") < 0){
+		fprint(2, "paint: initdraw failed: %r\n");
+		exits("initdraw");
+	}
+	einit(Emouse | Ekeyboard);
+	draw(screen, screen->r, display->white, 0, ZP);
+	flushimage(display, 1);
+
+	colors[0] = display->black;
+	colors[1] = display->white;
+	colors[2] = allocimage(display, Rect(0,0,1,1), CMAP8, 1, DRed);
+	colors[3] = allocimage(display, Rect(0,0,1,1), CMAP8, 1, DGreen);
+	colors[4] = allocimage(display, Rect(0,0,1,1), CMAP8, 1, DBlue);
+	colors[5] = allocimage(display, Rect(0,0,1,1), CMAP8, 1, DYellow);
+
+	ARGBEGIN{
+	default:
+		goto Usage;
+	}ARGEND
+	switch(argc){
+	default:
+	Usage:
+		fprint(2, "Usage: [file]\n");
+		exits("usage");
+	case 0:
+		break;
+	case 1:
+		strncpy(file, argv[0], sizeof(argv[0]));
+		if(loadimg(file) < 0)
+			sysfatal("%r");
+		break;
+	}
+
+	while(1){
+		switch(event(&e)){
+		case Emouse:
+			if(e.mouse.buttons & 1){
+				if(haslast)
+					line(screen, last, e.mouse.xy, Enddisc, Enddisc, b, colors[c], ZP);
+				else
+					fillellipse(screen, e.mouse.xy, b, b, colors[c], ZP);
+				last = e.mouse.xy;
+				haslast = 1;
+				flushimage(display, 1);
+			} else
+				haslast = 0;
+			break;
+		case Ekeyboard:
+			if(e.kbdc == 'b'){
+				if(eenter("Brush", brush, sizeof(brush), &e.mouse) <= 0)
+					break;
+				b = atoi(brush);
+			}
+			if(e.kbdc == 'c'){
+				if(eenter("Color", color, sizeof(color), &e.mouse) <= 0)
+					break;
+				cn = atoi(color);
+				if(cn >= 0 && cn < NCOLORS)
+					c = cn;
+			}
+			if(e.kbdc == 'f'){
+				if(eenter("Fill", fill, sizeof(fill), &e.mouse) <= 0)
+					break;
+				f = atoi(fill);
+				if(f >= 0 && f < NCOLORS)
+					draw(screen, screen->r, colors[f], 0, ZP);
+			}
+			if(e.kbdc == 'o'){
+				if(eenter("Open file", file, sizeof(file), &e.mouse) <= 0)
+					break;
+				if(loadimg(file) < 0){
+					rerrstr(file, sizeof(file));
+					eenter(file, 0, 0, &e.mouse);
+				}
+			}
+			if(e.kbdc == 'q')
+				exits(nil);
+			if(e.kbdc == 's'){
+				if(eenter("Save to", file, sizeof(file), &e.mouse) <= 0)
+					break;
+				if(saveimg(file) < 0){
+					rerrstr(file, sizeof(file));
+					eenter(file, 0, 0, &e.mouse);
+				}
+			}
+			break;
+		}
+	}
+}
---
+++ sys/src/cmd/uhtml.c
@@ -0,0 +1,169 @@
+#include <u.h>
+#include <libc.h>
+#include <ctype.h>
+
+int nbuf;
+char buf[64*1024+1];
+char *cset = nil;
+
+void
+usage(void)
+{
+	fprint(2, "%s [ -p ] [ -c charset ] [ file ]\n", argv0);
+	exits("usage");
+}
+
+char*
+attr(char *s, char *a)
+{
+	char *e, q;
+
+	if((s = cistrstr(s, a)) == nil)
+		return nil;
+	s += strlen(a);
+	while(strchr("\r\n\t ", *s))
+		s++;
+	if(*s++ != '=')
+		return nil;
+	while(strchr("\r\n\t ", *s))
+		s++;
+	q = 0;
+	if(*s == '"' || *s == '\'')
+		q = *s++;
+	for(e = s; *e; e++){
+		if(*e == q)
+			break;
+		if(isalnum(*e))
+			continue;
+		if(*e == '-' || *e == '_')
+			continue;
+		break;
+	}
+	if(e - s > 1)
+		return smprint("%.*s", (int)(e-s), s);
+	return nil;
+}
+
+void
+main(int argc, char *argv[])
+{
+	int n, pfd[2], pflag = 0;
+	char *arg[4], *s, *e, *p, *g, *a, t;
+	Rune r;
+
+	ARGBEGIN {
+	case 'c':
+		cset = EARGF(usage());
+		break;
+	case 'p':
+		pflag = 1;
+		break;
+	default:
+		usage();
+	} ARGEND;
+
+	if(*argv){
+		close(0);
+		if(open(*argv, OREAD) != 1)
+			sysfatal("open: %r");
+	}
+	nbuf = 0;
+	p = buf;
+	g = buf;
+	while(nbuf < sizeof(buf)-1){
+		if((n = read(0, buf + nbuf, sizeof(buf)-1-nbuf)) <= 0)
+			break;
+		nbuf += n;
+		buf[nbuf] = 0;
+		if(nbuf == n){
+			if(memcmp(p, "\xEF\xBB\xBF", 3)==0){
+				p += 3;
+				cset = "utf";
+				break;
+			}
+			if(memcmp(p, "\xFE\xFF", 2) == 0){
+				p += 2;
+				cset = "unicode-be";
+				break;
+			}
+			if(memcmp(p, "\xFF\xFE", 2) == 0){
+				p += 2;
+				cset = "unicode-le";
+				break;
+			}
+		}
+		s = g;
+		do {
+			if((s = strchr(s, '<')) == nil)
+				break;
+			g = s;
+			if((e = strchr(++s, '>')) == nil)
+				e = buf+nbuf;
+			t = *e;
+			*e = 0;
+			if((a = attr(s, "encoding")) || (a = attr(s, "charset"))){
+				cset = a;
+				*e = t;
+				break;
+			}
+			*e = t;
+			s = ++e;
+		} while(t);
+	}
+	nbuf -= p - buf;
+
+	if(cset == nil){
+		cset = "utf";
+		s = p;
+		while(s+UTFmax < p+nbuf){
+			s += chartorune(&r, s);
+			if(r == Runeerror){
+				cset = "latin1";
+				break;
+			}
+		}
+	}
+
+	if(pflag){
+		print("%s\n", cset);
+		exits(0);
+	}
+
+	if(nbuf == 0){
+		write(1, p, 0);
+		exits(0);
+	}
+
+	if(pipe(pfd) < 0)
+		sysfatal("pipe: %r");
+
+	switch(rfork(RFFDG|RFREND|RFPROC)){
+	case -1:
+		sysfatal("fork: %r");
+	case 0:
+		dup(pfd[0], 0);
+		close(pfd[0]);
+		close(pfd[1]);
+
+		arg[0] = "rc";
+		arg[1] = "-c";
+		arg[2] = smprint("{tcs -f %s || cat} | tcs -f html", cset);
+		arg[3] = nil;
+		exec("/bin/rc", arg);
+	}
+
+	dup(pfd[1], 1);
+	close(pfd[0]);
+	close(pfd[1]);
+
+	while(nbuf > 0){
+		if(write(1, p, nbuf) != nbuf)
+			sysfatal("write: %r");
+		p = buf;
+		if((nbuf = read(0, p, sizeof(buf))) < 0)
+			sysfatal("read: %r");
+	}
+	close(1);
+	waitpid();
+	exits(0);
+}
