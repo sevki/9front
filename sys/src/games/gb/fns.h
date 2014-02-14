74eac0acdf6beb829766140fb836125434ceff07
Matthew Veety <mveety@gmail.com>
2014-02-14 00:11:11 -0500

    similar to my last commit, but combined the two waits for the usb devices. still configurable, but with usbwait. If usbwait is not defined then it defaults to the original 2 seconds.

---
+++ sys/src/games/gb/fns.h
@@ -0,0 +1,11 @@
+u8int memread(u16int);
+void memwrite(u16int, u8int);
+int step(void);
+void ppustep(void);
+void disasm(u16int);
+void interrupt(u8int);
+void message(char *, ...);
+void flushram(void);
+void savestate(char *);
+void loadstate(char *);
+void initaudio(void);
