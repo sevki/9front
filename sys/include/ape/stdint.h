a13a920925a1f40a9e928b7c83d6155179b74c85
cinap_lenrek <cinap_lenrek@felloff.net>
2019-09-07 02:13:35 +0200

    merge

--- sys/include/ape/stdint.h
+++ sys/include/ape/stdint.h
@@ -1,8 +1,14 @@
-#ifndef _STDINT_H_
-#define _STDINT_H_ 1
+#ifndef _STDINT_GENERIC_H_
+#define _STDINT_GENERIC_H_ 1
 
+/*
+ * Default for 32 bit architectures, overriden by
+ * /$objtype/include/ape/stdint.h if needed.
+ */
+#ifndef _STDINT_ARCH_H_
 typedef int _intptr_t;
 typedef unsigned int _uintptr_t;
+#endif
 
 typedef char int8_t;
 typedef short int16_t;
@@ -12,6 +18,7 @@ typedef unsigned char uint8_t;
 typedef unsigned short uint16_t;
 typedef unsigned int uint32_t;
 typedef unsigned long long uint64_t;
+
 typedef _intptr_t intptr_t;
 typedef _uintptr_t uintptr_t;
 
@@ -19,6 +26,11 @@ typedef _uintptr_t uintptr_t;
 #define INT16_MIN	0x8000
 #define INT32_MIN	0x80000000
 #define INT64_MIN	0x8000000000000000LL
+
+#define UINT8_MIN	0
+#define UINT16_MIN	0 
+#define UINT32_MIN	0 
+#define UINT64_MIN	0
 
 #define INT8_MAX	0x7f
 #define INT16_MAX	0x7fff
@@ -29,5 +41,12 @@ typedef _uintptr_t uintptr_t;
 #define UINT16_MAX	0xffff
 #define UINT32_MAX	0xffffffffL
 #define UINT64_MAX	0xffffffffffffffffULL
+
+/* 
+ * Right now, all of our size_t types are 32 bit, even on
+ * 64 bit architectures.
+ */
+#define SIZE_MIN	UINT32_MIN
+#define SIZE_MAX	UINT32_MAX
 
 #endif
